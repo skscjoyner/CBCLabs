85b1f463fe141bc4e97ae84f75b57363ee474bb0
Shawn <backtothen@gmail.com>
2018-03-14 14:59:03 -0400

    Photo backend added

--- Photo-Upload/app/Http/Controllers/PhotoController.php
+++ Photo-Upload/app/Http/Controllers/PhotoController.php
@@ -6,39 +6,31 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Response;
 use Purifier;
-
 use App\Photo;
 
 class PhotoController extends Controller
 {
-    public function store(Request $request)
-    {
-      $rules = [
-        'photo' => 'required'
-      ];
+  public function store(Request $request)
+  {
+    $rules =[
+      'photo' => 'required'
+    ];
 
-      $validator = Validator::make(Purifier::clean($request->all()), $rules);
+  $validator = Validator::make(Purifier::clean($request->all()), $rules);
 
-      if($validator->fails())
-      {
-        return Response::json(['error' => 'Please fill out all fields.']);
-      }
+  if($validator->fails())
+  {
+    return Response::json(['error' => 'Please fill out all fields.']);
+  }
+  $photoInput = $request->file('photo');
+  $photoName = $photoInput->getClientOriginalName();
+  $photoInput->move("storage/", $photoName);
 
-      $photoInput = $request->file('photo');
-      $photoName = $photoInput->getClientOriginalName();
-      $photoInput->move("storage/", $photoName);
+  $photo = new Photo;
+  $photo->photoURL = $request->root()."/storage/".$photoName;
+  $photo->save();
 
-      $photo = new Photo;
-      $photo->photoURL = $request->root(). "/storage/".$photoName;
-
-      $success = $photo->save();
-      if ($success) {
-        return Response::json(['success' => 'Your photo was Uploaded.']);
-      } else {
-        return Response::json(['error' => 'Database errpr please try again.']);
-      }
-
-    }
-
+  return Response::json(['success'=> 'Your photo was uploaded.']);
+  }
 
 }
